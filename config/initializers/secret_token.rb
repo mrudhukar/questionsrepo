2ffe156657a7321c97daa1592686b699eee61a3b
mrudhukar <mrudhukar@chronus.com>
2012-11-17 16:43:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrepo::Application.config.secret_token = 'db2574be20dc0dda045491b5bad29fcad5a291efaf42d986e27676e60a2a30f862cfd04b2bb36b3b8e491d23a7bef1d3d084b556068963947fc529cb78955046'
